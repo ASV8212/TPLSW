e08fca5db63cc7c07159bff4b4dcb1734407c43f
ASV8212 <srinivash02@gmail.com>
2023-02-27 13:04:08 +0530

    Push
    push

--- src/main/java/APIInterface/InprincFlowHandler.java
+++ src/main/java/APIInterface/InprincFlowHandler.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package APIInterface;
 
 import java.sql.Connection;
@@ -63,69 +62,3 @@ public class InprincFlowHandler {
 		return respflg+"~"+resp;
 	}
 }
-=======
-package APIInterface;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
-import javax.servlet.http.HttpServletRequest;
-import CommonModel.DBConnection;
-
-
-
-
-public class InprincFlowHandler {
-	public static String inprincFlowHandler(HttpServletRequest request) {
-		String resp = "";
-		String respflg = "";
-		Connection con = null;
-		PreparedStatement procstmt = null;
-		ResultSet rs = null;
-		try
-		{
-			String xml = request.getParameter("Gridxml");
-			String prcsID = request.getParameter("prcsID");
-			con = DBConnection.getConnection(null);
-			procstmt = con.prepareStatement("{ call LSW_SINPRINCFLOW_HANDLR (?,?) }");
-			procstmt.setString(1, xml);
-			procstmt.setString(2, prcsID);
-
-			rs = procstmt.executeQuery();
-			while (rs.next()) {
-				resp = rs.getString("RESULT");
-				respflg = "Success";
-			}
-		}
-		catch(Exception e)
-		{
-			resp = e.getMessage();
-			respflg = "Fail";
-		}
-		finally {
-			try
-			{
-				if(con != null)
-				{
-					con.close();
-				}
-				if(procstmt != null)
-				{
-					procstmt.close();
-				}
-				if(rs != null)
-				{
-					rs.close();
-				}
-			}
-			catch(Exception e)
-			{
-				resp = e.getMessage();
-				respflg = "Fail";
-			}
-		}
-		return respflg+"~"+resp;
-	}
-}
->>>>>>> 7d3942bc6a8605ab885cdb80ab9877f7cad404e3
--- src/main/java/APIInterface/LeadSQR.java
+++ src/main/java/APIInterface/LeadSQR.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package APIInterface;
 
 import java.util.ArrayList;
@@ -59,65 +58,3 @@ public class LeadSQR {
 		return resp;
 	}
 }
-=======
-package APIInterface;
-
-import java.util.ArrayList;
-
-import javax.servlet.http.HttpServletRequest;
-
-import Controls.GetDBData;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-
-public class LeadSQR {
-	private LeadSQR() {
-		throw new IllegalStateException("Utility class");
-	}
-
-	public static String leadSQRStatusPush(HttpServletRequest req) {
-		String resp = "";
-		String msg = "";
-		String flg = "";
-		String tokn = "";
-		try {
-			String url = "";
-			String payload = "";
-			
-			String prcsid = req.getParameter("PrcsID");
-			String statusEvnt = req.getParameter("StatusEvnt");
-
-			ArrayList<String> data = new ArrayList<String>();
-			data = GetDBData.Call(prcsid, statusEvnt, "", "", "", "LSW_SSTATUSPUSHLEADSQRCREATE");
-			url = data.get(0);
-			payload = data.get(1);
-			tokn = data.get(2);
-
-			OkHttpClient client = new OkHttpClient().newBuilder().build();
-			MediaType mediaType = MediaType.parse("application/json");
-			RequestBody body = RequestBody.create(mediaType,payload);
-			Request request = new Request.Builder().url(url)
-					.method("POST", body).addHeader("Content-Type", "application/json").build();
-			Response response = client.newCall(request).execute();
-			if (response.code() == 200) {
-				flg = "Success";
-				msg = response.body().string();
-			} else {
-				flg = "Error";
-				msg = response.body().string();
-			}
-			response.body().close();
-		} catch (Exception e) {
-			flg = "Error";
-			msg = e.getMessage();
-			e.printStackTrace();
-		}
-		resp = flg + "~" + msg;
-		GetDBData.Call("", "", "", resp, tokn, "LSW_SSTATUSPUSHLEADSQRCREATE_UPD");
-		return resp;
-	}
-}
->>>>>>> 7d3942bc6a8605ab885cdb80ab9877f7cad404e3
