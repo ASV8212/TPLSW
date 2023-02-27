ca0d1e9eb0affecd212a40c06ff39de0cc845bed
ASV8212 <srinivash02@gmail.com>
2023-02-27 14:51:10 +0530

    TVS LOCAL
    LOCAL

--- src/main/java/CommonModel/DBConnection.java
+++ src/main/java/CommonModel/DBConnection.java
@@ -168,6 +168,7 @@ public class DBConnection {
 		InputStream inputStream;
 		Properties prop = new Properties();
 		FileName=System.getProperty("catalina.base");
+		
 		FileName=FileName+"/conf/"+propFileName;
 		//System.out.println(FileName);		
 		inputStream = new FileInputStream(FileName);
